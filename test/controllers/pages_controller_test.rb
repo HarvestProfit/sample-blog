5d6149937d275fc3116b010545c76e54a363aec9
Jaryd Krishnan <KrishnanJarydR@JohnDeere.com>
2021-04-05 15:44:24 -0500

    Add a home page for our API-only app

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get the home page" do
+    get root_url
+    assert_response :success
+  end
+end
