01d660030cb7378dd0f00039216303980c385f73
Chris Salazar <salazarchristophera@johndeere.com>
2021-07-01 12:02:31 -0400

    add tests for post controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,37 +2,71 @@ require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @post = posts(:post_one)
+    @user = User.create(email: "test@harvestprofit.com",
+                        username: "testaccount", 
+                        password: "testpass",
+                        password_confirmation: "testpass")
+
+    # @post = posts(:post_one, user: @user)
+    @post_params = {post: {content_markdown: "A quick example blog postsdfsdasfsdaf chrissdfsd324f23s edited new onesdffdsfd", description: "This is an awesome descriptionlol", image_url: "https://images.unsplash.com/photo-1592136187769-65d502332dc6", title: "New Post Title"}}
+    @authorization = JsonWebToken.encode(user_id: @user.id)
   end
 
   test "should get index" do
-    get posts_url, as: :json
-    assert_response :success
+    2.times { create_post(@post_params) }
+
+    get posts_url, headers: {Authorization: @authorization}, as: :json
+    response_json = JSON.parse(response.body)
+
+    assert(response_json.size == 2)
   end
 
   test "should create post" do
     assert_difference("Post.count") do
-      post posts_url, params: {post: {content_markdown: @post.content_markdown, description: @post.description, image_url: @post.image_url, title: @post.title}}, as: :json
+      create_post(@post_params)
     end
 
-    assert_response 201
+    post = get_post(response.body)
+
+    assert(@user.id == post.user_id)
   end
 
   test "should show post" do
-    get post_url(@post), as: :json
+    create_post(@post_params)
+    post = get_post(response.body)
+
+    get post_url(post), headers: {Authorization: @authorization}, as: :json
     assert_response :success
   end
 
   test "should update post" do
-    patch post_url(@post), params: {post: {content_markdown: @post.content_markdown, description: @post.description, image_url: @post.image_url, title: @post.title}}, as: :json
-    assert_response 200
+    post_params_2 = {post: {content_markdown: "A quick example blog postsdfsdasfsdaf chrissdfsd324f23s edited new onesdffdsfd", description: "This is an awesome descriptionlol", image_url: "https://images.unsplash.com/photo-1592136187769-65d502332dc6", title: "EDITED TITLE"}}
+
+    create_post(@post_params)
+    post = get_post(response.body)
+
+    patch post_url(post), params: post_params_2, headers: {Authorization: @authorization}, as: :json
+    post.reload
+
+    assert(post.title == "EDITED TITLE")
   end
 
   test "should destroy post" do
+    create_post(@post_params)
+    post = get_post(response.body)
+
     assert_difference("Post.count", -1) do
-      delete post_url(@post), as: :json
+      delete post_url(post), headers: {Authorization: @authorization}, as: :json
     end
 
     assert_response 204
+  end
+
+  def get_post(response)
+    Post.find(JSON.parse(response)['id'])
+  end
+
+  def create_post(params)
+    post posts_url, params: params, headers: {Authorization: @authorization}, as: :json
   end
 end
